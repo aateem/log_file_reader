deb3a3d034e89d3e3e28a448c55f0a3df6c586ce
Artem Roma <aroma@ws014.(none)>
2013-03-22 18:35:55 +0200

    At this moment - final version of my log files reader.

--- logfile_reader.py
+++ logfile_reader.py
@@ -2,11 +2,23 @@
 
 import time
 import re
+import datetime
 
 LOG_ENTRY_CHECK_PATTERN = "PID\s\(\d+\):\s((\d{4})(\-(\d{2})){2}\s((\d{2})\:){2}(\d{2}))"
 TIME_PARSE_PATTERN = "%Y-%m-%d %H:%M:%S"
-TIME_DELTA_LOG_ENTRY_FILTER = 5
 PATH_TO_LOG_FILE = ""
+TIME_BORDER = time.mktime((datetime.datetime.now() - datetime.timedelta(minutes=5)).timetuple())
+
+
+def time_check(matched_buff_entry):
+    '''
+        Compares time in form of unix time
+    '''
+    time_in_string = matched_buff_entry.groups()[0]
+    time_structure = time.strptime(time_in_string, TIME_PARSE_PATTERN)
+    log_entry_unix_time = time.mktime(time_structure)
+
+    return log_entry_unix_time >= TIME_BORDER
 
 
 def read_and_check(fileobj, fileobj_size, buf_ind):
@@ -29,12 +41,7 @@ def is_considerable_inside_buff(log_entry):
     if not matched:
         return False
 
-    time_from_log_entry = time.strptime(matched.groups()[0], TIME_PARSE_PATTERN)
-    current_time = time.localtime()
-
-    timedelta = current_time.tm_min - time_from_log_entry.tm_min
-
-    return timedelta <= TIME_DELTA_LOG_ENTRY_FILTER
+    return time_check(matched)
 
 
 def is_considerable_buff_edge(log_entry, log_chunk):
@@ -49,18 +56,13 @@ def is_considerable_buff_edge(log_entry, log_chunk):
         log_chunk_index += 1
         return is_considerable_buff_edge(log_chunk[log_chunk_index], log_chunk)
 
-    time_from_log_entry = time.strptime(matched.groups()[0], TIME_PARSE_PATTERN)
-    current_time = time.localtime()
-
-    timedelta = current_time.tm_min - time_from_log_entry.tm_min
-
-    if not(timedelta <= TIME_DELTA_LOG_ENTRY_FILTER):
+    if time_check(matched):
         return log_chunk[log_chunk_index:]
 
     return []
 
 
-def freader(path_to_file):
+def freader(path_to_file, error_displaying=False):
     # amount of read lines to be displayed
     chunk_lines = []
 
@@ -69,10 +71,11 @@ def freader(path_to_file):
         fsize = f.tell()  # get size of file
         buf_ind = 1  # for maintaining count of buffers to read
 
-        # time difference for log entry retrieving
-        #past_time_edge = (datetime.datetime.now() + datetime.timedelta(minutes=-5)).strftime('%Y-%m-%d %H:%M:%S')
-
         lines = read_and_check(f, fsize, buf_ind)
+
+        if not error_displaying:
+            chunk_lines = is_considerable_buff_edge(lines[0], lines)
+            return chunk_lines
 
         while True:
 
